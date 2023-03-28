5261b84f1eba918ac63bb84da8c84f482d77ea92
MTaimoor12345 <muhammad.taimoor525@gmail.com>
2023-03-28 13:42:58 +0500

    up

--- app.js
+++ app.js
@@ -9,21 +9,24 @@ setInterval(function date1(){
     var sess = document.getElementById("session");
     var ab = hour - 12 ;
 
- if (hour > 12){
-  hour = ab ;
- }
+
+ if (hour >= 12){
+    sess.innerText = "PM";
+   
+}else{
+    sess.innerText = "AM";
+}
+
+if (hour > 12){
+    hour = ab ;
+   }
 
     document.getElementById("hour").innerText = hour;
     document.getElementById("min").innerText = min;
     document.getElementById("sec").innerText = sec;
 
 
-    if (hour >= 12){
-        sess.innerText = "PM";
-       
-    }else{
-        sess.innerText = "AM";
-    }
+   
     
 
 
